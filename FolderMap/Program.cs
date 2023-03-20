58d9a0fbab575ba761361d74796f653ed2ad89bd
lass132d <lass132d@edu.campusvejle.dk>
2023-03-20 10:35:36 +0100

    added foldermap assignment

---
+++ FolderMap/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FolderMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirSearch("C:\\Users\\lass132d\\Desktop\\V");
+
+            void DirSearch(string sDir)
+            {
+
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    Console.WriteLine("Folder: " + d + "\n");
+                    foreach (string f in Directory.GetFiles(sDir))
+                    {
+                        Console.WriteLine(f);
+                    }
+                    DirSearch(d);
+                }
+
+            }
+        }
+    }
+}
