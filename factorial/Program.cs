ac9a8c86417a7d4d7c5af940cad04eb73a374f1f
lass132d <lass132d@edu.campusvejle.dk>
2023-03-20 10:25:49 +0100

    Add project files.

---
+++ factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(ToFactorialRecu(5));
+
+            //iterative
+
+            int ToFactorialRecu(int number)
+            {
+                if (number > 1)
+                {
+                    return number *= ToFactorialRecu(number - 1);
+                }
+                return number;
+            }
+        }
+    }
+}
---
+++ fibonachi/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Schema;
+
+namespace fibonachi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FibRecuFirstNth(27);
+
+            // opg 1
+            //recursive fib(extremely inefficient implimentation)
+            int FibRecu(int number)
+            {
+                if (number < 2) return number;
+
+                return FibRecu(number - 1) + FibRecu(number - 2);
+            }
+
+            //opg 2
+            void FibRecuFirstNth(int number)
+            {
+                for(int i = 0; i < number; i++)
+                {
+                    Console.WriteLine(FibRecu(i));
+                }
+            }
+
+            //iterative fib
+            int FibIter(int number)
+            {
+                Console.WriteLine(0);
+                long current = 1;
+                long last = 0;
+                long temp;
+                for (int i = 2; i < number; i++)
+                {
+                    Console.WriteLine(current);
+                    temp = current;
+                    current += last;
+                    last = temp;
+                }
+
+                return Convert.ToInt32(current);
+            }
+        }
+    }
+}
