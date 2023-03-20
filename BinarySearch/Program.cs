83481ff04d77b5d3227541043c8c1295e92c350d
lass132d <lass132d@edu.campusvejle.dk>
2023-03-20 13:30:29 +0100

    working product

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -17,11 +17,11 @@ namespace BinarySearch
             Array.Sort(sortedArr);
 
 
-
+            //array for easy testing
             int[] testArr = new int[100] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100};
 
             //write method result
-            Console.WriteLine(BinarySearch(55, testArr));
+            Console.WriteLine(BinarySearch(99, testArr));
 
             //medthod for ease of use for users
             int BinarySearch(int target, int[] arr)
@@ -38,22 +38,22 @@ namespace BinarySearch
                 Console.WriteLine("search: " + (max - (max - min) / 2));
 
                 //gets mid between min and max and checks if its the target number in the array
-                if (arr[max - (max - min) / 2] == target)
+                if (arr[max - ((max - min) / 2)] == target)
                 {
                     return max - (max - min) / 2;
                 }
                 else if(min != max)
                 {
                     //target is in lower half
-                    if (arr[(max - min) / 2] > target)
+                    if (arr[max - ((max - min) / 2)] > target)
                     {
-                        max = (max - min) / 2;
+                        max = max - (max - min) / 2;
                         return BinarySearchRecu(target, arr, min, max);
                     }
                     //target in upper half
                     else
                     {
-                        min = (max - min) / 2;
+                        min = max - ((max - min) / 2);
                         return BinarySearchRecu(target, arr, min, max);
                     }
                 }
