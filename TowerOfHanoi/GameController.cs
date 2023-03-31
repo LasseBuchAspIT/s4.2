671909a64adbe7181e72bb64658e6d184f473980
lass132d <lass132d@edu.campusvejle.dk>
2023-03-31 09:08:25 +0200

    final touches

--- TowerOfHanoi/GameController.cs
+++ TowerOfHanoi/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,16 @@ namespace TowerOfHanoi
         int ringCount;
         private Tower[] towers = new Tower[3] { new(), new(), new() };
         private Ring SelectedRing;
-        private Tower SelectedTower;
+        private int SelectedTower; 
+        private int SelectedRingTower;
+        private string msg = "";
 
 
         public GameController(int ringCount)
         {
             this.ringCount = ringCount;
             SquareSize = ringCount + 4; 
-            SelectedTower = towers[0];
+            SelectedTower = 0;
 
             for(int i = ringCount + 2; i >= 1; i -= 2)
             {
@@ -30,6 +33,7 @@ namespace TowerOfHanoi
         public void Draw()
         {
             Console.Clear();
+            Console.WriteLine(msg);
             Console.WriteLine();
             Console.WriteLine();
             for (int i  = ringCount; i > 0; i--)
@@ -43,7 +47,7 @@ namespace TowerOfHanoi
                         {
                             Console.ForegroundColor = ConsoleColor.Green;
                         }
-                        else if(t == SelectedTower)
+                        else if(t == towers[SelectedTower])
                         {
                             Console.ForegroundColor = ConsoleColor.Red;
                         }
@@ -68,13 +72,12 @@ namespace TowerOfHanoi
             {
                 from.moveTop(to);
                 //if success redraw scene
-                Draw();
 
             }
             catch
             {
 
-                Console.WriteLine("Cant place on top of smaller ring");
+                msg = "Cant place on top of smaller ring";
             }
              
         }
@@ -88,8 +91,44 @@ namespace TowerOfHanoi
             {
                 //Draw scene
                 Draw();
+                ConsoleKey action = Console.ReadKey().Key;
+                if(action == ConsoleKey.Enter)
+                {
+                    if(SelectedRing != null)
+                    {
+                        towers[SelectedRingTower].moveTop(towers[SelectedTower]);
+                        SelectedRing = null;
 
-
+                    }
+                    else
+                    {
+                        SelectedRing = towers[SelectedTower].rings.Last();
+                        SelectedRingTower = SelectedTower;
+                    }
+                }
+                else if(action == ConsoleKey.RightArrow)
+                {
+                    if(SelectedTower >= 2)
+                    {
+                        msg = "cant go any further right";
+                    }
+                    else
+                    {
+                        SelectedTower++;
+                    }
+                }
+                else if(action == ConsoleKey.LeftArrow)
+                {
+                    if (SelectedTower == 0)
+                    {
+                        msg = "cant go any further left";
+                    }
+                    else
+                    {
+                        SelectedTower--;
+                    }
+                }
+                msg = "";
             }
         }
 
