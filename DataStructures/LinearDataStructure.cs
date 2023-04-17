3356a29a06475ac5eca4e952b654d9d56ad3b787
Lasse Emil Buch <lass132d@edu.campusvejle.dk>
2023-04-17 09:57:09 +0200

    this project sucks for i am dumb

--- DataStructures/LinearDataStructure.cs
+++ DataStructures/LinearDataStructure.cs
@@ -5,20 +5,56 @@ namespace DataStructures
     public abstract class LinearDataStructure<T>
     {
         T[] data;
-        public int Length;
-        public int Count;
+        protected int length;
+        protected int count;
+        protected int elements;
 
         protected LinearDataStructure(int initialLength)
         {
             data = new T[initialLength];
+            length = initialLength;
         }
 
-        public void Insert(T itemToInsert, int index)
+        protected void Insert(T itemToInsert, int index)
         {
             data[index] = itemToInsert;
+
+            if (!data.Contains(itemToInsert))
+            {
+                count++;
+            }
+                elements++;
         }
 
-        private void MoveAll(int from)
+        //moving method
+        //useful for inserting
+        protected void MoveAll(int from)
+        {
+            T Last = default(T);
+            if(from < length)
+            {
+                if(elements >= length)
+                {
+
+                }
+
+                
+               for(int i = from; from < elements + 1; i++)
+               {
+                    T tmp = data[i];
+                    data[i] = Last;
+                    Last = tmp;
+               }
+            }
+
+        }
+
+        protected void ReSize(int newSize)
+        {
+
+        }
+
+        protected void Remove(int index)
         {
 
         }
